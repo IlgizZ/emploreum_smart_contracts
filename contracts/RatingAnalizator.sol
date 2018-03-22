94358d1840161dcf22ddffb36bb6b8fc0faa116d
do6ryak <ilgiz1996@mail.ru>
2018-03-22 20:00:33 +0300

    add analizator

---
+++ contracts/RatingAnalizator.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.11;
+import "./Employee.sol";
+import "./Company.sol";
+
+
+contract RatingAnalizator {
+
+    function RatingAnalizator() public {
+    }
+
+    function () public payable {
+        revert();
+    }
+
+    function companyRating(Company company, address[] employees) {
+        
+
+    }
+
+
+}
